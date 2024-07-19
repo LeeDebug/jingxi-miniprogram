1c9de814a52a1a20b461265d4077a806d8143fff
Edan's Win11 <961150665@qq.com>
2024-07-19 22:57:05 +0800

    fix: 会员等级

--- pages/ucenter/mumber/index.js
+++ pages/ucenter/mumber/index.js
@@ -1,6 +1,7 @@
 Page({
   data: {
     DotStyle: true,
+    defaultLevel: 1,
     cardCur: 0,
     swiperList: [
       {
@@ -41,8 +42,12 @@ Page({
     ],
   },
   onLoad() {
-    this.towerSwiper('swiperList');
-    // 初始化towerSwiper 传已有的数组名即可
+    // TODO: 想把当前等级设置为默认轮播，但失败了
+    // setTimeout(() => {
+    //   this.setData({
+    //     cardCur: this.data.defaultLevel
+    //   })
+    // }, 10);
   },
   // cardSwiper
   cardSwiper(e) {
@@ -50,58 +55,4 @@ Page({
       cardCur: e.detail.current
     })
   },
-  // towerSwiper
-  // 初始化towerSwiper
-  towerSwiper(name) {
-    let list = this.data[name];
-    for (let i = 0; i < list.length; i++) {
-      list[i].zIndex = parseInt(list.length / 2) + 1 - Math.abs(i - parseInt(list.length / 2))
-      list[i].mLeft = i - parseInt(list.length / 2)
-    }
-    this.setData({
-      swiperList: list
-    })
-  },
-  // towerSwiper触摸开始
-  towerStart(e) {
-    this.setData({
-      towerStart: e.touches[0].pageX
-    })
-  },
-  // towerSwiper计算方向
-  towerMove(e) {
-    this.setData({
-      direction: e.touches[0].pageX - this.data.towerStart > 0 ? 'right' : 'left'
-    })
-  },
-  // towerSwiper计算滚动
-  towerEnd(e) {
-    let direction = this.data.direction;
-    let list = this.data.swiperList;
-    if (direction == 'right') {
-      let mLeft = list[0].mLeft;
-      let zIndex = list[0].zIndex;
-      for (let i = 1; i < list.length; i++) {
-        list[i - 1].mLeft = list[i].mLeft
-        list[i - 1].zIndex = list[i].zIndex
-      }
-      list[list.length - 1].mLeft = mLeft;
-      list[list.length - 1].zIndex = zIndex;
-      this.setData({
-        swiperList: list
-      })
-    } else {
-      let mLeft = list[list.length - 1].mLeft;
-      let zIndex = list[list.length - 1].zIndex;
-      for (let i = list.length - 1; i > 0; i--) {
-        list[i].mLeft = list[i - 1].mLeft
-        list[i].zIndex = list[i - 1].zIndex
-      }
-      list[0].mLeft = mLeft;
-      list[0].zIndex = zIndex;
-      this.setData({
-        swiperList: list
-      })
-    }
-  }
 })
