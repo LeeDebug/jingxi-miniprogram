acb6bcd0d48fee351b88fe11a5ded95d663f22d8
Edan's Win11 <961150665@qq.com>
2024-07-19 21:39:08 +0800

    fix: 修改 鲸禧控卡 字样

--- pages/index/index.js
+++ pages/index/index.js
@@ -60,7 +60,7 @@ Page({
     onShareAppMessage: function () {
         let info = wx.getStorageSync('userInfo');
         return {
-            title: '海风小店',
+            title: '鲸禧控卡',
             desc: '开源微信小程序商城',
             path: '/pages/index/index?id=' + info.id
         }
