d093d7f02a0e1e83057172a249da48f53d333659
Edan's Win11 <961150665@qq.com>
2024-07-26 17:01:40 +0800

    fix: 修改首页布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,141 +1,136 @@
+
 const util = require('../../utils/util.js');
 const api = require('../../config/api.js');
-const user = require('../../services/user.js');
-
-//获取应用实例
-const app = getApp()
 
 Page({
-    data: {
-        floorGoods: [],
-        openAttr: false,
-        showChannel: 0,
-        showBanner: 0,
-        showBannerImg: 0,
-        banner: [],
-        index_banner_img: 0,
-        userInfo: {},
-        imgurl: '',
-        sysHeight: 0,
-        loading: 0,
-        autoplay: true,
-        showContact: 1,
-    },
-    onLoad: function (options) {
-        this.getChannelShowInfo();
-    },
-    onPageScroll: function (e) {
-        let scrollTop = e.scrollTop;
-        let that = this;
-        if (scrollTop >= 2000) {
-            that.setData({
-                showContact: 0
-            })
-        } else {
-            that.setData({
-                showContact: 1
-            })
-        }
-    },
-    onHide: function () {
-        this.setData({
-            autoplay: false
-        })
-    },
-    goSearch: function () {
-        wx.navigateTo({
-            url: '/pages/search/search',
-        })
-    },
-    goCategory: function (e) {
-        let id = e.currentTarget.dataset.cateid;
-        wx.setStorageSync('categoryId', id);
-        wx.switchTab({
-            url: '/pages/category/index',
-        })
-    },
-    handleTap: function (event) {
-        //阻止冒泡 
-    },
-    onShareAppMessage: function () {
-        let info = wx.getStorageSync('userInfo');
-        return {
-            title: '鲸禧控卡',
-            desc: '开源微信小程序商城',
-            path: '/pages/index/index?id=' + info.id
-        }
-    },
-    toDetailsTap: function () {
-        wx.navigateTo({
-            url: '/pages/goods-details/index',
-        });
-    },
-    getIndexData: function () {
-        let that = this;
-        util.request(api.IndexUrl).then(function (res) {
-            if (res.errno === 0) {
-                that.setData({
-                    floorGoods: res.data.categoryList,
-                    banner: res.data.banner,
-                    channel: res.data.channel,
-                    notice: res.data.notice,
-                    loading: 1,
-                });
-                let cartGoodsCount = '';
-                if (res.data.cartCount == 0) {
-                    // wx.removeTabBarBadge({
-                    //     index: 2,
-                    // })
-                } else {
-                    cartGoodsCount = res.data.cartCount + '';
-                    // wx.setTabBarBadge({
-                    //     index: 2,
-                    //     text: cartGoodsCount
-                    // })
-                }
-            }
-        });
-    },
 
-    onShow: function () {
-        this.getIndexData();
-        var that = this;
-        let userInfo = wx.getStorageSync('userInfo');
-        if (userInfo != '') {
-            that.setData({
-                userInfo: userInfo,
-            });
-        };
-        let info = wx.getSystemInfoSync();
-        let sysHeight = info.windowHeight - 100;
-        this.setData({
-            sysHeight: sysHeight,
-            autoplay: true
+  data: {
+    // 滚动公告
+    Headlines: [
+      {
+        id: 0,
+        title: "下单前要先看好下周菜单哦~ (点击即可查看)",
+        url: '/pages/seasonal-menu/index'
+      },
+      {
+        id: 1,
+        title: "优惠券中心上线啦，快去抢几张~",
+        url: '/pages/ucenter/coupons/my'
+      }
+    ],
+    banner: [],
+    userInfo: {},
+    // 决定 loading 圈的高度
+    sysHeight: 0,
+    loading: 0,
+    autoplay: true,
+    showContact: 1,
+  },
+
+  onLoad: function (options) {
+  },
+
+  onShow: function () {
+    this.getIndexData();
+
+    var that = this;
+    let userInfo = wx.getStorageSync('userInfo');
+    if (userInfo != '') {
+        that.setData({
+            userInfo: userInfo,
         });
-        wx.removeStorageSync('categoryId');
-    },
-    getChannelShowInfo: function (e) {
-        let that = this;
-        util.request(api.ShowSettings).then(function (res) {
-            if (res.errno === 0) {
-                let show_channel = res.data.channel;
-                let show_banner = res.data.banner;
-                let show_notice = res.data.notice;
-                let index_banner_img = res.data.index_banner_img;
-                that.setData({
-                    show_channel: show_channel,
-                    show_banner: show_banner,
-                    show_notice: show_notice,
-                    index_banner_img: index_banner_img
-                });
-            }
+    };
+    let info = wx.getSystemInfoSync();
+    let sysHeight = info.windowHeight - 100;
+    this.setData({
+        sysHeight: sysHeight,
+        autoplay: true
+    });
+    wx.removeStorageSync('categoryId');
+  },
+
+  onPageScroll: function (e) {
+    let scrollTop = e.scrollTop;
+    let that = this;
+    if (scrollTop >= 2000) {
+        that.setData({
+            showContact: 0
+        })
+    } else {
+        that.setData({
+            showContact: 1
+        })
+    }
+  },
+
+  onHide: function () {
+      this.setData({
+          autoplay: false
+      })
+  },
+
+  onShareAppMessage: function () {
+      let info = wx.getStorageSync('userInfo');
+      return {
+          title: '鲸禧控卡',
+          desc: '开源微信小程序商城',
+          path: '/pages/index/index?id=' + info.id
+      }
+  },
+
+  /**
+   * 触发 下拉刷新动作
+   */
+  onPullDownRefresh () {
+    // wx.showNavigationBarLoading()
+    this.getIndexData();
+    wx.hideNavigationBarLoading() //完成停止加载
+    wx.stopPullDownRefresh() //停止下拉刷新
+  },
+
+  getIndexData () {
+      let that = this;
+      // 即 index/appInfo 接口
+      util.request(api.IndexUrl).then(function (res) {
+          if (res.errno === 0) {
+              that.setData({
+                  // floorGoods: res.data.categoryList,
+                  banner: res.data.banner,
+                  // channel: res.data.channel,
+                  // notice: res.data.notice,
+                  loading: 1,
+              });
+              let cartGoodsCount = '';
+              if (res.data.cartCount == 0) {
+                  // wx.removeTabBarBadge({
+                  //     index: 2,
+                  // })
+              } else {
+                  cartGoodsCount = res.data.cartCount + '';
+                  // wx.setTabBarBadge({
+                  //     index: 2,
+                  //     text: cartGoodsCount
+                  // })
+              }
+          }
+      });
+  },
+
+  /**
+   * 点击轮播公告，跳转相应页面
+   * @param {*} e 
+   */
+  linesclick(e) {
+    var swip = e.currentTarget.dataset
+    // console.log(swip);
+    wx.switchTab({
+      url: swip.url,
+      fail() {
+        wx.navigateTo({
+          url: swip.url
         });
-    },
-    onPullDownRefresh: function () {
-        wx.showNavigationBarLoading()
-        this.getIndexData();
-        this.getChannelShowInfo();
-        wx.hideNavigationBarLoading() //完成停止加载
-        wx.stopPullDownRefresh() //停止下拉刷新
-    },
+      }
+    })
+  },
+
 })
